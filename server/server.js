c2bdaf064b63f65f2c8b25f469192db4a8903da4
shreshth <shreshthsrivastava2@gmail.com>
2020-10-03 22:48:24 -0400

    got google oauth working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const passportSetup = require('./passport-config');
 const officialsController = require('./officialsController.js');
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Respond with index.html file when user opens the page
 app.get('/', (req, res) => {
@@ -16,6 +22,23 @@ app.get('/', (req, res) => {
 app.post('/officials', officialsController.getOfficials, (req, res) => {
   res.status(200).json(res.locals.officialsData);
 });
+
+// Initial OAuth route
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile'],
+  })
+);
+
+// Route that Google redirects to
+app.get(
+  '/auth/google/redirect',
+  passport.authenticate('google'),
+  (req, res) => {
+    res.send('oauth complete');
+  }
+);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
